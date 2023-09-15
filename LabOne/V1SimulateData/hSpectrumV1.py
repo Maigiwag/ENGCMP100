cc4500922c7d14667ecf84913cc568ede14f7b30
Jason Wang <j.wang73@share.espb.ca>
2023-09-14 23:51:46 -0600

    finished v1

--- LabOne/V1SimulateData/hSpectrumV1.py
+++ LabOne/V1SimulateData/hSpectrumV1.py
@@ -12,7 +12,7 @@
 # Electrical and Computer Engineering
 # All rights reserved.
 #
-# Student name: Jason Wang 
+# Student name: Jason Wang (100%)
 # Student CCID: jason20 
 # Others: 
 #
@@ -32,22 +32,43 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-## EXPERIMENT DATA
-#
-data = [656.460,486.271,434.1692,410.2892] # nm
-nist = np.array(data)
-n = len(nist)
 
 ## MODEL SETUP
 #
-rydberg = 1.0973732e7 # 1/m
-print("Rydberg constant:",rydberg)
+elecMass = 9.1093837e-31 #kg
+fundCharge = 1.6021766e-19 #C
+permOfSpace =  8.8541878e-12 #m^-3 kg^-1 s^4 A^2
+planck = 6.6260702e-34 #Joule seconds
+SOL =  2.9979246e8 #m/s
+rydberg = (elecMass*(fundCharge**4))/(8*(permOfSpace**2)*(planck**3)*SOL)
+rydberg = int(rydberg)
+print(f"Rydberg Constant: {rydberg} 1/m ")
 
-## SIMULATION DATA
+## SIMULATION/EXPERIMENT DATA       
+# I combined both simulation and experiement because their both needed to help calculate bohr data
+# this way all the relevant data is combined into one section of the code
 #
+data = [656.460,486.271,434.1692,410.2892,397.1198,389.0166,383.6485] # nm
+nist = np.array(data)         #data given by lab
+n = len(nist)
 nf = input("Final state (nf): ")
-nf = int(nf)
+nf = int(nf)                  #all this is doing is getting the nf form the user and using that to find ni
 ni = np.arange(nf+1,nf+n+1)
-plt.plot(ni,nist,'bx')
+waveL = 1/(rydberg*((1/nf**2)-(1/ni**2))) #using data from above to calculate bohr data
+waveL = np.divide(waveL, (10**-9)) #making the data from above into nanometers this allows it to be plotted correctly
+
+#this section gathers and calculates data used to then make the graph created in the next section
+
+
+##Plotting 
+plt.plot(ni,nist,'bx', ms = 8)
+plt.title("Hydrogen Emission Spectrum")
+plt.xlabel("Initial state (ni)")
+plt.ylabel("Wavelength (nm)")
+plt.plot(ni,waveL,'ro') #plots the bohr model data
+plt.legend(['NIST data', 'Bohr model'])
 plt.grid(True)
 plt.show()
+
+#i serperated the plotting into its own section because i need nf and ni
+# isolated to calcualte bohr data
