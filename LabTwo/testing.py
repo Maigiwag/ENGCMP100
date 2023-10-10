1342c3e49aaf54d4d89770dd6a05c685f024b956
Jason Wang <j.wang73@share.espb.ca>
2023-10-10 12:28:22 -0600

    Lab3

--- LabTwo/testing.py
+++ LabTwo/testing.py
@@ -1,23 +1,15 @@
-# root thingie mabob
-
 import numpy as np
 
-a = float(input('coeff a: '))
-b = float(input('coeff b: '))
-c = float(input('coeff c: '))
-D = b**2-4*a*c #discriminant
-if D<0:
-    print("no real roots")
-else:
-    r = float(input('guess: '))
-    if np.abs(a*r**2+b*r+c)<1e-16:
-        print("Your guess was indeed a root!")
-    else:
-        if a == 0:
-            x = -c/b
-            print(f"there is one root it is : {x}")
-        else:
-            r1 = (-b+np.sqrt(D)/(2*a))
-            r2 = (-b-np.sqrt(D)/(2*a))
-            print(f"the roots are {r1} and {r2}")
+nmax = 40 
+oddInt = np.arange(1,nmax,2)
+print(oddInt)
+n = len(oddInt)
+alt = (-1)**np.arange(0,n)
+print(alt)
+lebinizVector = 1/oddInt*alt
+print(lebinizVector)
+sum = 0
+for el in lebinizVector:
+    sum = sum + el
 
+print(sum)
