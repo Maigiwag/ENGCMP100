f75c0954d9efb98fb85fa0a8451afedcb20a4897
Jason Wang <j.wang73@share.espb.ca>
2023-10-10 15:04:20 -0600

    finished v1

--- LabThree/V1Saving/V1Saving/lab3V1.py
+++ LabThree/V1Saving/V1Saving/lab3V1.py
@@ -2,9 +2,10 @@
 # Electrical and Computer Engineering
 # All rights reserved.
 #
-# Student name:
-# Student CCID:
-# Others:
+# Student name: jason wang (99%)
+# Student CCID: jason20
+# Others: Farhan Zaman (1%)
+#   MatPlotLib Documentation was also refferecned to create the x axis
 #
 # To avoid plagiarism, list the names of persons, Version 0 author(s)
 # excluded, whose code, words, ideas, or data you used. To avoid
@@ -22,12 +23,54 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-print('Version 1')
+print('Version 1 - Solution')
 # ------------Students edit/write their code below here--------------------------
+Saving = [2000]
+artTuit = [5550]
+sciTuit = [6150]
+engTuit = [6550]
+years = np.arange(0,19)
+artFinalTuit = 0
+sciFinalTuit= 0
+engFinalTuit = 0
+
+
+for i in range(216):
+    Saving += [(Saving[i]+(Saving[i]*(0.0625/12)))+200]
+
+for i in range(21):
+    artTuit += [artTuit[i]+(artTuit[i]*0.07)]
+    sciTuit += [sciTuit[i]+(sciTuit[i]*0.07)]
+    engTuit += [engTuit[i]+(engTuit[i]*0.07)]
+
+for i in range(4):
+    i = (i+1)*-1
+    
+    artFinalTuit += artTuit[i]
+    sciFinalTuit += sciTuit[i]
+    engFinalTuit += engTuit[i]
+
+print(f'''
+The savings amount is ${format(Saving[215], '.2f')}
+The cost of the Arts program is ${format(artFinalTuit,'.2f')}
+The cost of the Science program is ${format(sciFinalTuit, '.2f')}
+The cost of the Engineering program is ${format(engFinalTuit, '.2f')}
+''')
+
 # ------------Remove any code that is unnecessary--------------------------
-counter = []
-for i in range(0, 216):
-    counter += [i]
-print(f'counter(last value) = {counter[-1]} \n')
-plt.plot(range(0, 216), counter)
+
+
+fig,ax = plt.subplots()
+ax.plot(Saving)
+ax.hlines(artFinalTuit,0 ,216, colors = 'orange')
+ax.hlines(sciFinalTuit,0 ,216, colors = 'g')
+ax.hlines(engFinalTuit,0 ,216, colors = 'r')
+plt.title("Savings Vs Tuition")
+plt.axis([0,217,0,100000])
+ax.set(xticks= range(0,217,12), xticklabels=years)
+plt.xlabel("Years")
+plt.ylabel("Amount $")
+plt.legend(['Savings Balance', 'Arts', 'Science', 'Engineering'], loc = 'lower right')
 plt.show()
+
+
