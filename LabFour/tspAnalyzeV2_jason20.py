399bea59b57214326d7499443b828b382b7c0cc2
Jason Wang <j.wang73@share.espb.ca>
2023-10-30 17:21:00 -0600

    finished all the code

--- LabFour/tspAnalyzeV2_jason20.py
+++ LabFour/tspAnalyzeV2_jason20.py
@@ -31,7 +31,7 @@
 # source to the lab instructor before the assignment is due.
 #
 
-##---VERSION ONE---##
+##---VERSION TWO---##
 
 import scipy.io as io
 import numpy as np
@@ -44,21 +44,24 @@ def main():
     file = open('tspAbout.txt','r')
     print(file.read())
     file.close()
-
-    choice = menu()
-
+    choice = menu() 
     while choice != 0:
         if choice == 1:
-
             tspPrint(tsp)
-
+        elif choice == 2:
+            minVal,maxVal = tspMinMax(tsp)
+            tsp = tspLimit(tsp,minVal,maxVal)
         elif choice == 3:
-
             tspPlot(tsp)
 
         choice = menu()
 
+
+# Menu function displays all the necessary input information to the user 
+# this function takes no input 
+# however it will return the users input as an int after verifying if its valid 
 def menu():
+        numError = True
         print()
         print("MAIN MENU")
         print("0. Exit program")
@@ -66,11 +69,14 @@ def menu():
         print("2. Limit dimension")
         print("3. Plot one tour")
         print()
-        choice = int(input("Choice (0-3)? "))
-        while not (0 <= choice <= 3):
-            choice = int(input("Choice (0-3)? "))
+        while numError == True or choice < 0 or choice > 3: 
+            choice = input("Choice (0-3)? ")
+            choice,numError = checkInt(choice)
         return choice
 
+#this funciton will display all avaliable datasets to attempt to create a plot with
+#it takes one input which is the tspData file stored in a variable
+#there is no return value however it will display text to the user 
 def tspPrint(tsp):
     print()
     print("NUM  FILE NAME  EDGE TYPE  DIMENSION  COMMENT")
@@ -82,8 +88,15 @@ def tspPrint(tsp):
         print("%3d  %-9.9s  %-9.9s  %9d  %s"
             % (k,name,edge,dimension,comment))
 
+#asks the user for a specific dataset to attempt to create a plot for 
+#it takes one input which is the tspDAta file stored in a variable 
+#if the data is able to be plotted it will then send the required info to the plotEuc2D function
+#there is no return output however it will display text to the user
 def tspPlot(tsp):
-        num = int(input("Number (EUC_2D)? "))
+        numError = True
+        while numError == True or num < 1 or num > (len(tsp)):
+            num = input("Number (EUC_2D)? ")
+            num,numError = checkInt(num)
         edge = tsp[num][5]
         tsp1 = tsp[num]
         if edge == 'EUC_2D':
@@ -92,6 +105,10 @@ def tspPlot(tsp):
         else:
             print("Invalid (%s)!!!" % edge)
 
+#plots the given information using matplotlib and then stores the plot as a png
+#it takes the cords, name, and other required information for a given dataset
+#using the inputted informaiton it then produces a graph that gets saved as a png
+#there is no return output 
 def plotEuc2D(coord, comment, name):
     xPlot = []
     yPlot = []
@@ -103,10 +120,50 @@ def plotEuc2D(coord, comment, name):
     plt.title(comment)
     plt.xlabel("x-Cordinate")
     plt.ylabel("y-Cordinate")
-    plt.legend([name],loc = 'upper right')
+    plt.legend([name])
     plt.savefig('tspPlot.png')
     plt.clf()
 
-          
+
+def tspMinMax(tsp):
+    minVal = int(tsp[1][3])
+    maxVal = int(tsp[1][3])
+    for i in range(1,len(tsp)):
+        val = int(tsp[i][3])
+        if val > maxVal:
+            maxVal = val
+        elif val < minVal:
+            minVal = val 
+    print(f'''Min dimension: {minVal}
+Max dimension: {maxVal}''')   
+    return minVal,maxVal
+
+
+def tspLimit(tsp,minVal,maxVal):
+    newtsp = [tsp[0]]
+    numError = True
+    while numError == True or limit < minVal or limit > maxVal:
+        limit = input("Limit value? ")
+        limit,numError = checkInt(limit)
+    maxVal = limit
+    for k in range(1,len(tsp)):
+        val = int(tsp[k][3])
+        if val <= maxVal:
+            newtsp.append(tsp[k])
+    return newtsp
+
+
+#this funciton will check if a input is a valid integer so that the program will not crash
+#it accepts an variable of an unknown datatype
+#it returns a int or a false 
+def checkInt(num):
+    try:
+        num = int(num)
+        error = False
+    except:
+        error = True
+    return num,error
+            
+
 
 main()
